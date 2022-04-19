40aae3d78390c5ba9c426920dac60f7b8fd09b2e
Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
2022-04-19 13:11:46 +0700

    set config

---
+++ backend/pkg/utils/configs/environment_struct.go
@@ -0,0 +1,21 @@
+package configs
+
+type environment uint8
+
+const (
+	dev environment = iota
+	prod
+)
+
+func (v environment) String() string {
+	return [...]string{"dev", "prod"}[v]
+}
+
+func (v *environment) Set(string string) {
+	switch string {
+	case "dev": // 0 is dev
+		*v = dev
+	case "prod":
+		*v = prod
+	}
+}
