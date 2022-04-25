61fef965b6bbf22dd4ace0c540ab47af1a8e680f
Yok-Thanawan <thanawan.li@mail.kmutt.ac.th>
2022-04-25 23:42:24 +0700

    add profilepage, listitem widget

--- frontend/lib/screens/profile/profile_page.dart
+++ frontend/lib/screens/profile/profile_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:paotung_frontend/screens/main/mainpage.dart';
+import 'package:paotung_frontend/widgets/common/app_bar.dart';
+import '../../widgets/common/list_item.dart';
+import '../../widgets/main/profile/profile.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
@@ -11,7 +15,18 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("My page")),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 40.0),
+          child: Column(
+            children: [
+              profileSection(),
+              SizedBox(height: 60,),
+              listItem(title: "Category Setting", routes: MainPage()),
+              listItem(title: "Notification Setting", routes: MainPage()),
+              listItem(title: "Help Center", routes: MainPage()),
+            ],
+          ),
+        ),
     );
   }
 }
--- frontend/lib/widgets/main/profile/profile.dart
+++ frontend/lib/widgets/main/profile/profile.dart
@@ -1,1 +1,39 @@
-// 
+//
+import 'package:flutter/material.dart';
+import 'package:paotung_frontend/constants/theme.dart';
+
+class profileSection extends StatefulWidget {
+  const profileSection({Key? key}) : super(key: key);
+
+  @override
+  State<profileSection> createState() => _profileSectionState();
+}
+
+class _profileSectionState extends State<profileSection> {
+  String username = "Barbie Roberts";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          maxRadius: 40.0,
+          child: const Text('user'),
+        ),
+        Padding(padding: EdgeInsets.symmetric(vertical: 25, horizontal: 12)),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              username,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16,),
+            Text("Edit Profile",style: TextStyle(color: AppColors.grey),),
+          ],
+        ),
+      ],
+    ));
+  }
+}
