a03a27b0550838aff393956fe3407e039312cdb2
muumel <mel.austapinya@gmail.com>
2022-04-27 15:20:13 +0700

    move barchart folder
    move barchart folder to common widgets

--- frontend/lib/screens/main/dashborad.dart
+++ frontend/lib/screens/main/dashborad.dart
@@ -2,8 +2,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:paotung_frontend/constants/theme.dart';
 import 'package:paotung_frontend/widgets/common/overview_pie_chart.dart';
-
-import '../../widgets/bar_chart.dart';
+import '../../widgets/common/bar_chart.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
